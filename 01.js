d73dcb0b8b6bbad4b83776f6b879156dcd78a121
Viktor <364185@protonmail.com>
2023-02-19 14:00:57 +0400

    2

--- 01.js
+++ 01.js
@@ -6,3 +6,20 @@ const printReversedWordBySymbol = (name) => {
     }
 };
 console.log(printReversedWordBySymbol('12345'))
+
+//-------------------------------------
+const countChars = (str, char) => {
+    let i = 0;
+    let count = 0;
+    while (i < str.length) {
+        if (str[i].toLowerCase() === char.toLowerCase()) {
+            // Считаем только подходящие символы
+            count = count + 1;
+        }
+        // Счетчик увеличивается в любом случае
+        i = i + 1;
+    }
+
+    return count;
+};
+console.log(countChars('FEar cuts deeper than swords.', 'E'))
